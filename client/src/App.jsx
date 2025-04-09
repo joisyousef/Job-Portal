15f3f24d057598bbe32bce2ec42bc6ecc5436d49
joisyousef <yousefelsrogy500@gmail.com>
2025-04-09 18:08:24 +0200

    Add Auths

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import ApplyJob from "./pages/ApplyJob";
-import Applications from "./pages/Applications";
+import Applications from "./pages/Application";
 
 const App = () => {
   return (
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,17 +1,33 @@
 import React from "react";
 import { assets } from "../assets/assets";
+import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
 
 const Navbar = () => {
+  const { openSignIn } = useClerk();
+  const { user } = useUser();
+
   return (
     <div className="shadow py-4">
       <div className="container px-4 2xl:px-0 mx-auto flex justify-between items-center">
         <img src={assets.logo} />
-        <div>
-          <button className="text-grey-600">Login</button>
-          <button className="bg-blue-600 text-white px-6 sm:px-9 py-2 rounded-full">
-            Recruter Login
-          </button>
-        </div>
+        {user ? (
+          <div>
+            <Link to={"/applications"}>Applied Jobs</Link>
+            <p>|</p>
+            <p>Hi, {user.firstName + " " + user.lastName}</p>
+            <UserButton />
+          </div>
+        ) : (
+          <div>
+            <button className="text-gray-600">Recruter Login</button>
+            <button
+              onClick={(e) => openSignIn()}
+              className="bg-blue-600 text-white px-6 sm:px-9 py-2 rounded-full"
+            >
+              Login
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
