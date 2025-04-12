ac2c609d512f93d8d3144521ad57588894523a78
joisyousef <yousefelsrogy500@gmail.com>
2025-04-12 18:47:48 +0200

    add css to hero section

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom"; // Added import for Link
 import { assets } from "../assets/assets";
 import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
 
@@ -9,19 +10,19 @@ const Navbar = () => {
   return (
     <div className="shadow py-4">
       <div className="container px-4 2xl:px-0 mx-auto flex justify-between items-center">
-        <img src={assets.logo} />
+        <img src={assets.logo} alt="Logo" />
         {user ? (
-          <div>
-            <Link to={"/applications"}>Applied Jobs</Link>
+          <div className="flex items-center gap-3">
+            <Link to="/applications">Applied Jobs</Link>
             <p>|</p>
             <p>Hi, {user.firstName + " " + user.lastName}</p>
             <UserButton />
           </div>
         ) : (
           <div>
-            <button className="text-gray-600">Recruter Login</button>
+            <button className="text-gray-600">Recruiter Login</button>
             <button
-              onClick={(e) => openSignIn()}
+              onClick={openSignIn}
               className="bg-blue-600 text-white px-6 sm:px-9 py-2 rounded-full"
             >
               Login
@@ -32,4 +33,5 @@ const Navbar = () => {
     </div>
   );
 };
+
 export default Navbar;
