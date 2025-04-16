ab41954e761f05394f01dd651787d6ff90256a49
joisyousef <yousefelsrogy500@gmail.com>
2025-04-16 14:09:03 +0200

    add joblisting

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
+import Joblisting from "../components/JobListing";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Joblisting />
     </div>
   );
 };
