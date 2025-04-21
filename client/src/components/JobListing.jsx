9ed4d4a983de324faf577a79600851299d80e42b
joisyousef <yousefelsrogy500@gmail.com>
2025-04-21 17:26:56 +0200

    Done With Pagination

--- client/src/components/JobListing.jsx
+++ client/src/components/JobListing.jsx
@@ -7,7 +7,9 @@ const JobListing = () => {
   const { isSearched, searchFilter, setSearchFilter, jobs } =
     useContext(AppContext);
 
-  const [showFilter, setShowFilter] = useState(true);
+  const [showFilter, setShowFilter] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+
   return (
     <div className="container 2xl:px-20 mx-auto flex flex-col lg:flex-row space-y-8 lg:space-y-0 py-8">
       {/* Sidebar */}
@@ -86,11 +88,52 @@ const JobListing = () => {
         <p className="mb-8">Get your desired job from top companies</p>
         <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-4">
           {jobs.length > 0 ? (
-            jobs.map((job) => <JobCard key={job.id || job.title} job={job} />)
+            jobs
+              .slice((currentPage - 1) * 6, currentPage * 6)
+              .map((job) => <JobCard key={job.id || job.title} job={job} />)
           ) : (
             <p>No jobs match your search criteria.</p>
           )}
         </div>
+        {/* Pagination */}
+        {jobs.length > 0 && (
+          <div className="flex items-center justify-center space-x-2 mt-10">
+            <a href="">
+              <img
+                onClick={() => setCurrentPage(Math.max(currentPage - 1), 1)}
+                src={assets.left_arrow_icon}
+                alt=""
+              />
+            </a>
+            {Array.from({ length: Math.ceil(jobs.length / 6) }).map(
+              (_, index) => (
+                <a href="#job-list">
+                  <button
+                    onClick={() => setCurrentPage(index + 1)}
+                    className={`w-10 h-10 flex items-center justify-center border border-gray-300 rounded ${
+                      currentPage === index + 1
+                        ? "bg-blue-100 text-blue-500"
+                        : "text-gray-500"
+                    }`}
+                  >
+                    {index + 1}
+                  </button>
+                </a>
+              )
+            )}
+            <a href="#job-list">
+              <img
+                onClick={() =>
+                  setCurrentPage(
+                    Math.min(currentPage + 1, Math.ceil(jobs.length / 6 - 1))
+                  )
+                }
+                src={assets.right_arrow_icon}
+                alt=""
+              />
+            </a>
+          </div>
+        )}
       </section>
     </div>
   );
