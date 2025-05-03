a946fb761f9400649ae0de0e818bcb8e02cd6938
joisyousef <yousefelsrogy500@gmail.com>
2025-05-03 15:26:22 +0300

    fixed app.jsx to render dashboard pages correctly

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { React, useContext } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import ApplyJob from "./pages/ApplyJob";
 import Applications from "./pages/Application";
@@ -12,19 +12,33 @@ import ViewApplication from "./pages/ViewApplication";
 
 const App = () => {
   const { showRecruiterLogin } = useContext(AppContext);
+
   return (
     <div>
       {showRecruiterLogin && <RecruitersLogin />}
       <Routes>
+        {/* Public routes */}
         <Route path="/" element={<Home />} />
         <Route path="/apply-job/:id" element={<ApplyJob />} />
         <Route path="/applications" element={<Applications />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/add-job" element={<AddJob />} />
-        <Route path="/manage-jobs" element={<ManageJobs />} />
-        <Route path="/view-application/:id" element={<ViewApplication />} />
+
+        {/* Dashboard with nested routes */}
+        <Route path="/dashboard" element={<Dashboard />}>
+          {/* Redirect from /dashboard to /dashboard/add-job */}
+          <Route index element={<Navigate to="/dashboard/add-job" replace />} />
+
+          {/* Nested dashboard routes */}
+          <Route path="add-job" element={<AddJob />} />
+          <Route path="manage-jobs" element={<ManageJobs />} />
+          <Route path="view-applications" element={<ViewApplication />} />
+          <Route path="view-application/:id" element={<ViewApplication />} />
+        </Route>
+
+        {/* Catch-all route - redirect to home */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </div>
   );
 };
+
 export default App;
