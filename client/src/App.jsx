d67a0a3cec84f2fb691c5469a535777663367417
joisyousef <yousefelsrogy500@gmail.com>
2025-05-03 06:34:10 +0300

    starting in dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,28 @@
-import React from "react";
+import { React, useContext } from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import ApplyJob from "./pages/ApplyJob";
 import Applications from "./pages/Application";
+import RecruitersLogin from "./components/RecruitersLogin";
+import { AppContext } from "./context/AppContext";
+import Dashboard from "./pages/Dashboard";
+import AddJob from "./pages/AddJob";
+import ManageJobs from "./pages/ManageJobs";
+import ViewApplication from "./pages/ViewApplication";
 
 const App = () => {
+  const { showRecruiterLogin } = useContext(AppContext);
   return (
     <div>
+      {showRecruiterLogin && <RecruitersLogin />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/apply-job/:id" element={<ApplyJob />} />
         <Route path="/applications" element={<Applications />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/add-job" element={<AddJob />} />
+        <Route path="/manage-jobs" element={<ManageJobs />} />
+        <Route path="/view-application/:id" element={<ViewApplication />} />
       </Routes>
     </div>
   );
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import { React, useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { assets } from "../assets/assets";
 import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
+import { AppContext } from "../context/AppContext";
 
 const Navbar = () => {
   const { openSignIn } = useClerk();
   const { user } = useUser();
   const navigate = useNavigate();
+
+  const { setShowRecruiterLogin } = useContext(AppContext);
 
   return (
     <div className="shadow py-4">
@@ -14,7 +17,7 @@ const Navbar = () => {
         {/* Logo on the left */}
         <img
           onClick={() => navigate("/")}
-          className="cursor-pointer h-10 sm:h-14"
+          className="cursor-pointer h-10 sm:h-14 hover:scale-105 transition duration-300"
           src={assets.logo}
           alt="Logo"
         />
@@ -46,10 +49,17 @@ const Navbar = () => {
           </div>
         ) : (
           <div className="flex items-center gap-3">
-            <button className="text-gray-600">Recruiter Login</button>
+            <button
+              onClick={() => {
+                setShowRecruiterLogin(true);
+              }}
+              className="text-gray-600 cursor-pointer hover:underline hidden sm:block"
+            >
+              Recruiter Login
+            </button>
             <button
               onClick={openSignIn}
-              className="bg-blue-600 text-white px-6 sm:px-9 py-2 rounded-full"
+              className="bg-blue-600 text-white px-6 sm:px-9 py-2 rounded-full cursor-pointer hover:bg-blue-800 transition duration-300"
             >
               Login
             </button>
