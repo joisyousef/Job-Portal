f1f191685eae4c920a7576aba817b4c77788513d
joisyousef <yousefelsrogy500@gmail.com>
2025-05-04 15:16:12 +0300

    Completed all client side and starting in the server side

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import Dashboard from "./pages/Dashboard";
 import AddJob from "./pages/AddJob";
 import ManageJobs from "./pages/ManageJobs";
 import ViewApplication from "./pages/ViewApplication";
+import "quill/dist/quill.snow.css";
 
 const App = () => {
   const { showRecruiterLogin } = useContext(AppContext);
