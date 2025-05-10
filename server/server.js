6f802720e86adfa4547ec03c7a7547e0020ca61a
joisyousef <yousefelsrogy500@gmail.com>
2025-05-10 16:14:29 +0300

    Sentry configuration Added

--- server/server.js
+++ server/server.js
@@ -1,9 +1,15 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import connectDB from "./configs/db.js";
+import "./configs/instrument.js";
+import * as Sentry from "@sentry/node";
 
 // Initialize express app
 const app = express();
+
+// Database connection
+await connectDB();
 
 // Middlewares
 app.use(cors());
@@ -11,9 +17,14 @@ app.use(express.json());
 
 // Routes
 app.get("/", (req, res) => res.send("API Working"));
+// app.get("/debug-sentry", function mainHandler(req, res) {
+//   throw new Error("My first Sentry error!");
+// });
 
 // Port
 const PORT = process.env.PORT || 5000;
+// Sentry configuration
+Sentry.setupExpressErrorHandler(app);
 
 // Start server
 app.listen(PORT, () => {
