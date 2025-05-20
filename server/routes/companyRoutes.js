34cc00382f9b3e1216969f4a118e504f6d41a01d
joisyousef <yousefelsrogy500@gmail.com>
2025-05-20 22:04:02 +0300

    added protectCompany middleware to the routes

--- server/routes/companyRoutes.js
+++ server/routes/companyRoutes.js
@@ -10,6 +10,7 @@ import {
   postJob,
 } from "../controllers/companyController.js";
 import upload from "../configs/multer.js";
+import { protectCompany } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
@@ -20,21 +21,21 @@ router.post("/register", upload.single("image"), registerCompany);
 router.post("/login", loginCompany);
 
 // Get company data
-router.get("/company", getCompanyData);
+router.get("/company", protectCompany, getCompanyData);
 
 // Post a job
-router.post("/post-job", postJob);
+router.post("/post-job", protectCompany, postJob);
 
 // Get applicants data of company
-router.get("/applicants", getCompanyJobApplicants);
+router.get("/applicants", protectCompany, getCompanyJobApplicants);
 
 // Get company job list
-router.get("list-jobs", getCompanyPostedJobs);
+router.get("list-jobs", protectCompany, getCompanyPostedJobs);
 
 // Change application status
-router.post("/change-status", changeJobApplicationsStatus);
+router.post("/change-status", protectCompany, changeJobApplicationsStatus);
 
 // Change application visiblity
-router.post("/change-visiblity", changeVisiblity);
+router.post("/change-visiblity", protectCompany, changeVisiblity);
 
 export default router;
