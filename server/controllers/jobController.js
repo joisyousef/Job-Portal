ae8d6cf86fd23bc0da7b6b588ef57ef80bee2852
joisyousef <yousefelsrogy500@gmail.com>
2025-05-28 01:27:26 +0300

    add jobRoutes and jobController

---
+++ server/controllers/jobController.js
@@ -0,0 +1,4 @@
+// Get all jobs
+export const getJobs = async (req, res) => {};
+// Get a specific job by ID
+export const getJobById = async (req, res) => {};
---
+++ server/routes/jobRoutes.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const router = express.Router();
+
+// Route to get all jobs data
+
+// Route to get a specific job by ID
+
+export default router;
