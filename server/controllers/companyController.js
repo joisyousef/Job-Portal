442bca913831ef8201017ada7d20be121e748ab6
joisyousef <yousefelsrogy500@gmail.com>
2025-05-28 16:32:18 +0300

    getCompanyData Done

--- server/controllers/companyController.js
+++ server/controllers/companyController.js
@@ -83,7 +83,21 @@ export const loginCompany = async (req, res) => {
 };
 
 // Get company data
-export const getCompanyData = async (req, res) => {};
+export const getCompanyData = async (req, res) => {
+  try {
+    const company = req.company;
+
+    res.json({
+      success: true,
+      company,
+    });
+  } catch (error) {
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
 // Post a new job
 export const postJob = async (req, res) => {
