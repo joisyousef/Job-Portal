0b9a06b39d999c024d8220cb32ab8beaf0669397
joisyousef <yousefelsrogy500@gmail.com>
2025-05-28 22:42:05 +0300

    userRoutes & userController

---
+++ server/controllers/userController.js
@@ -0,0 +1,11 @@
+// Get User Data
+export const getUserData = async (req, res) => {};
+
+// Apply for a job
+export const applyForJob = async (req, res) => {};
+
+// Get user's applied jobs
+export const getUserJobApplication = async (req, res) => {};
+
+// Update User Profile (Resume)
+export const updateUserResume = async (req, res) => {};
---
+++ server/routes/userRoutes.js
@@ -0,0 +1,21 @@
+import express from "express";
+import {
+  applyForJob,
+  getUserData,
+  getUserJobApplication,
+  updateUserResume,
+} from "../controllers/userController.js";
+
+const router = express.Router();
+
+// Get User Data
+router.get("/", getUserData);
+
+// Apply for a job
+router.post("/apply", applyForJob);
+
+// Get user's applied jobs
+router.get("/applications", getUserJobApplication);
+
+// Update User Profile (Resume)
+router.post("/update-resume", upload.single("resume"), updateUserResume);
