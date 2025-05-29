3da5e8104010a66d97e3a55fd20c9f624e01f545
joisyousef <yousefelsrogy500@gmail.com>
2025-05-30 02:31:46 +0300

    applyForJob Done

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -21,7 +21,43 @@ export const getUserData = async (req, res) => {
 };
 
 // Apply for a job
-export const applyForJob = async (req, res) => {};
+export const applyForJob = async (req, res) => {
+  const { jobId } = req.body;
+  const userId = req.auth.userId;
+  try {
+    const isAlreadyApplied = await JobApplication.find({ jobId, userId });
+
+    if (isAlreadyApplied.length > 0) {
+      return res.json({
+        success: false,
+        message: "You have already applied for this job",
+      });
+    }
+    const jobData = await Job.findById(jobId);
+    if (!jobData) {
+      return res.json({
+        success: false,
+        message: "Job not found",
+      });
+    }
+    await JobApplication.create({
+      userId,
+      jobId,
+      CompanyId: jobData.CompanyId,
+      // status: "Pending",
+      date: Date.now(),
+    });
+    res.json({
+      success: true,
+      message: "Job application submitted successfully",
+    });
+  } catch (error) {
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
 // Get user's applied jobs
 export const getUserJobApplication = async (req, res) => {};
