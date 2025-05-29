506bb9f5d06ff66aa966ce2a5da850f19d716b5a
joisyousef <yousefelsrogy500@gmail.com>
2025-05-29 14:57:12 +0300

    getUserData

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,24 @@
+import { User } from "@clerk/express";
+
 // Get User Data
-export const getUserData = async (req, res) => {};
+export const getUserData = async (req, res) => {
+  const userId = req.auth.userId;
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.json({
+        success: false,
+        message: "User not found",
+      });
+      res.json({
+        success: true,
+        user,
+      });
+    }
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // Apply for a job
 export const applyForJob = async (req, res) => {};
